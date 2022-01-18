dce89a1738d92fe4444edd50e833ae90497c3f20
mplummer9519 <mplummer9519@gmail.com>
2022-01-17 16:09:54 -0800

    Added the filter static entry

--- test/main.py
+++ test/main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/")
 def GetIngressFilterData(filter_name):
     headers={"X-Auth-Token":"6cc3c907948d4ca3e92416067c08bc7fa57c7125"}
-    ingress_url="https://192.168.2.134/api/v1/ingress_whitelist/" + filter_name
+    ingress_url="https://192.168.2.134/api/v1/ingress_whitelist/SvchostFilter" + filter_name
     responses = requests.get(ingress_url,headers=headers,verify=False)        
     data = json.loads(responses.text)
     hit_rate_current = " "
