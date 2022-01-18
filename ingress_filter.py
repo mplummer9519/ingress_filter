de18e8f4180bd1c2c1262a0e45ed74f7c34d2273
mplummer9519 <mplummer9519@gmail.com>
2022-01-17 16:01:38 -0800

    made some changes to main

--- ingress_filter.py
+++ ingress_filter.py
@@ -15,28 +15,15 @@ import time
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def GetIngressFilterData(filter_name):
-
     headers={"X-Auth-Token":"6cc3c907948d4ca3e92416067c08bc7fa57c7125"}
     ingress_url="https://192.168.2.134/api/v1/ingress_whitelist/" + filter_name
-    responses = requests.get(ingress_url,headers=headers,verify=False)
-        
+    responses = requests.get(ingress_url,headers=headers,verify=False)        
     data = json.loads(responses.text)
     hit_rate_current = " "
-
-
     if responses.status_code == 200:
-       #print("success")
-
        hit_rate_current = data['hit_rate']
-
-    return hit_rate_current
-num = 1 
-while 0 < num:
-
-   current_rate = GetIngressFilterData("SvchostFilter")
-   print(current_rate)
-   time.sleep(5)
-
+   return hit_rate_current
+   
 
 
 
--- test/main.py
+++ test/main.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 def index():
     return "Congrats its a web app"
 
+@app.route("/")
+def GetIngressFilterData(filter_name):
+    headers={"X-Auth-Token":"6cc3c907948d4ca3e92416067c08bc7fa57c7125"}
+    ingress_url="https://192.168.2.134/api/v1/ingress_whitelist/" + filter_name
+    responses = requests.get(ingress_url,headers=headers,verify=False)        
+    data = json.loads(responses.text)
+    hit_rate_current = " "
+    if responses.status_code == 200:
+       hit_rate_current = data['hit_rate']
+       return str(hit_rate_current)
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
 
