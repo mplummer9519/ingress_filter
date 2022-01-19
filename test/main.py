81b02fdebcc2ac3ff6ccbabef18534eafe528b47
mplummer9519 <mplummer9519@gmail.com>
2022-01-18 16:32:37 -0800

    changed page name

--- test/main.py
+++ test/main.py
@@ -28,6 +28,13 @@ def name():
     return str(first_name)
 
 
+@app.route("/webpage")
+def webpage():
+    return render_template('test.html')
+
+
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
 
